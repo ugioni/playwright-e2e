6d180a5134b2072195a20294ef610841e8d94c89
Leandro Ugioni <leandro_ugioni@hotmail.com>
2022-04-24 23:10:51 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:playwright/playwright-test',
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-unresolved': ['error', { commonjs: true, amd: true }],
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'no-use-before-define': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
+};
---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: 'src/scenarios',
+  timeout: 600000,
+  retries: 0,
+  use: {
+    channel: 'msedge',
+    trace: 'on',
+    locale: 'pt-BR',
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    screenshot: 'on',
+    video: 'off'
+  },
+  expect: {
+    timeout: 30000
+  },
+  reporter: [
+    [
+      'html',
+      {
+        outputFolder: 'artifacts/report',
+        open: 'never'
+      }
+    ]
+  ]
+};
+export default config;
