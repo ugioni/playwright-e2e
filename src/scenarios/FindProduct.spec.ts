cd3abdedbbc5b09b4c2d700d21368d15373d262c
Leandro Ugioni <leandro_ugioni@hotmail.com>
2022-10-21 23:38:00 -0300

    [fix] change page objects

--- src/scenarios/FindProduct.spec.ts
+++ src/scenarios/FindProduct.spec.ts
@@ -4,17 +4,18 @@ import { TheConfig } from 'sicolo';
 import HomePage from '../support/pages/HomePage';
 
 test.describe('Find Products', () => {
+  let homePage: HomePage;
   const CONFIG = join(__dirname, '../support/fixtures/config.yml');
   const BASE_URL = TheConfig.fromFile(CONFIG)
     .andPath('application.base_url')
     .retrieveData();
 
   test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
     await page.goto(BASE_URL);
   });
 
-  test('Find product by name', async ({ page }) => {
-    const homePage = new HomePage(page);
+  test('Find product by name', async () => {
     await homePage.searchProductByName();
     await homePage.checkProductCount();
   });
