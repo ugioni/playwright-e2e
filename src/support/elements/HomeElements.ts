ad7363b28fe474097220b6fdd1ffba5a9a503fc7
Leandro Ugioni <leandro_ugioni@hotmail.com>
2024-10-15 21:16:18 -0300

    update project

--- src/support/elements/HomeElements.ts
+++ src/support/elements/HomeElements.ts
@@ -7,27 +7,27 @@ export default class HomeElements extends BaseElements {
     this.page = page;
   }
 
-  getBotaoPropietario(): Locator {
-    return this.page.locator(
-      'img[src="/Fernanda/Content/img/proprietario.png"]'
-    );
+  getSearchField(): Locator {
+    return this.page.locator('#search_query_top');
   }
 
-  getBotaoFazenda(): Locator {
-    return this.page.locator('img[src="/Fernanda/Content/img/fazenda.png"]');
+  getSearchButton(): Locator {
+    return this.page.locator('button[name="submit_search"]');
   }
 
-  getBotaoCultura(): Locator {
-    return this.page.locator('img[src="/Fernanda/Content/img/folha.png"]');
+  getProductCount(): Locator {
+    return this.page.locator('span[class="heading-counter"]');
   }
 
-  getBotaoGarantia(): Locator {
-    return this.page.locator(
-      'img[src="/Fernanda/Content/img/dinheiro-de-volta.png"]'
-    );
+  getLoginField(): Locator {
+    return this.page.locator('#user-name');
   }
 
-  getBotaoUsuarios(): Locator {
-    return this.page.locator('img[src="/Fernanda/Content/img/usuarios.png"]');
+  getPassField(): Locator {
+    return this.page.locator('#password');
+  }
+
+  getSubmitButton(): Locator {
+    return this.page.locator('#login-button');
   }
 }
